393f6e87240258217be27ce36d2a8494d3fd48a0
gustavoalves23 <gustavo.alves388@gmail.com>
2022-04-26 21:08:31 -0300

    feat: Finish lesson 3.2

---
+++ 3-FirstSteps/3.2-FirstCppProgram/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+
+  std::cout << "Number1" << std::endl;
+  std::cout << "Number2";
+  std::cout << "Number3" << std::endl;
+
+  return 0;
+}
